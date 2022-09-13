500d4cb71e381e343aea0f5c0db6fbe55ad9ae37
saqib ahmed <saqib.ahmed@m3tech.com.pk>
2022-09-13 11:13:46 +0500

    Add function

---
+++ SwiftyLib/SwiftyLib.swift
@@ -0,0 +1,23 @@
+//
+//  SwiftyLib.swift
+//  SwiftyLibExamples
+//
+//  Created by Jahangir Mirza on 13/09/2022.
+//
+
+import Foundation
+
+
+public final class SwiftyLib {
+
+    let name = "SwiftyLib"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+    
+}
